739f376b22daf01650e30cd7dd94781c19790972
tonyjhuang <huangtonyj91@gmail.com>
2018-11-02 08:21:58 -0700

    slightm modification to graph.rb

--- graph/solution/lib/graph.rb
+++ graph/solution/lib/graph.rb
@@ -13,13 +13,15 @@ class Edge
 
   def initialize(from_vertex, to_vertex, cost = 1)
     @from_vertex, @to_vertex, @cost = from_vertex, to_vertex, cost
-    from_vertex.out_edges << self
-    to_vertex.in_edges << self
+
+    @from_vertex.out_edges << self
+    @to_vertex.in_edges << self
   end
 
   def destroy!
-    from_vertex.out_edges.delete(self)
-    to_vertex.in_edges.delete(self)
+    @from_vertex.out_edges.delete(self)
+    @to_vertex.in_edges.delete(self)
+    
     @from_vertex = @to_vertex = nil
   end
 end
